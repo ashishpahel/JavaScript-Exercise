b324793169cb54c7e2d4a0e1652f96be5ac43da8
ashishpahel <ashishpahel1999@gmail.com>
2023-03-22 16:30:23 +0530

    solution first complete

---
+++ Exercise-1/solution1.js
@@ -0,0 +1,17 @@
+// Define a function that takes two parameters
+function sumNumbers(num1, num2) {
+  // Add the two parameters and store the result in a variable
+  const sum = num1 + num2;
+  // Return the sum
+  return sum;
+}
+
+// Call the function and output the result to the console
+console.log(sumNumbers(5, 10)); // Output: 15
+
+/*
+The function is named sumNumbers and takes two parameters, num1 and num2, which represent the two numbers to be added together.
+Inside the function, the two parameters are added using the + operator and the result is stored in a variable named sum.
+Finally, the sum variable is returned by the function using the return statement.
+To test the function, we can call it with two numbers as arguments and output the returned value to the console using console.log(). In this case, we are passing 5 and 10 as arguments, so the output will be 15.
+*/
