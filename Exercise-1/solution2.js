8dee2be4f9cdf8025605e1482d7132d08bd46443
ashishpahel <ashishpahel1999@gmail.com>
2023-04-11 00:18:15 +0530

    solution 2

---
+++ Exercise-1/solution2.js
@@ -0,0 +1,16 @@
+// Define a function that takes a string parameter
+function getStringLength(str) {
+    // Get the length of the string using the `.length` property
+    const length = str.length;
+    // Return the length of the string
+    return length;
+  }
+  
+  // Call the function and output the result to the console
+  console.log(getStringLength("Ashish Pahel")); // Output: 12
+
+
+//   The function is named getStringLength and takes one parameter, str, which represents the string whose length is to be determined.
+// Inside the function, the length of the string is obtained using the .length property, which returns the number of characters in the string.
+// Finally, the length of the string is returned by the function using the return statement.
+// To test the function, we can call it with a string as an argument and output the returned value to the console using console.log(). In this case, we are passing "Hello, world!" as the argument, so the output will be 13, which is the length of the string.
